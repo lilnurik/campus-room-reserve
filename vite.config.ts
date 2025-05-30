4cb07357c85e7a4a8e6518bacd4deb0b87d2bf43
Bo'riyev Nurmuxammad <108805831+lilnurik@users.noreply.github.com>
2025-05-30 17:45:35 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     proxy: {
       // Add this proxy configuration to resolve CORS issues
       '/api': {
-        target: 'http://127.0.0.1:5321',
+        target: 'https://room.turin.uz',
         changeOrigin: true,
         secure: false,
         // Configure proxy to pass through the Authorization header
